7b5311ce0eedde02d57a02f3c9b13c3acb8984d4
Jonathan Hou <houj@oxy.edu>
2019-09-17 23:28:08 -0700

    Added modeMapper and Address Class

--- src/main/java/com/appsdeveloperblog/app/ws/dto/UserDto.java
+++ src/main/java/com/appsdeveloperblog/app/ws/dto/UserDto.java
@@ -1,6 +1,9 @@
 package com.appsdeveloperblog.app.ws.dto;
 
 import java.io.Serializable;
+import java.util.List;
+
+import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
 
 public class UserDto implements Serializable {
 	private static final long serialVersionUID = 4865903039190150223L;
@@ -13,6 +16,7 @@ public class UserDto implements Serializable {
 	private String encryptedPassword;
 	private String emailVertificationToken;
 	private Boolean emailVerificationStatus = false;
+	private List<AddressDTO> addresses;
 
 	public long getId() {
 		return id;
@@ -84,6 +88,14 @@ public class UserDto implements Serializable {
 
 	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
 		this.emailVerificationStatus = emailVerificationStatus;
+	}
+
+	public List<AddressDTO> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<AddressDTO> addresses) {
+		this.addresses = addresses;
 	}
 
 }
--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -27,7 +28,7 @@ import com.appsdeveloperblog.app.ws.ui.model.response.RequestOperationStatus;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("users") //http://localhost:8080/mobile-app-ws/ , http://localhost:8080/shoe-store/
 public class UserController {
 	
 	@Autowired
@@ -71,8 +72,11 @@ public class UserController {
 		
 		if(userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 		
-		UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails,  userDto);
+		//UserDto userDto = new UserDto();
+		//BeanUtils.copyProperties(userDetails,  userDto);
+		
+		ModelMapper modelMapper = new ModelMapper();
+		UserDto userDto = modelMapper.map(userDetails,  UserDto.class);
 		
 		UserDto createdUser = userService.createUser(userDto);
 		BeanUtils.copyProperties(createdUser,  returnValue);
